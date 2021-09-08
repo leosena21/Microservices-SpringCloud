8a0114c9b12e28768399a47a49cf64891cc76587
Leonardo <leeosena21@gmail.com>
2021-09-08 14:58:14 -0300

    Ajustes na versão do Java + criação do Service

--- loja/src/main/java/br/com/microservico/loja/resource/CompraResource.java
+++ loja/src/main/java/br/com/microservico/loja/resource/CompraResource.java
@@ -1,19 +1,24 @@
 package br.com.microservico.loja.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.microservico.loja.dto.CompraDTO;
+import br.com.microservico.loja.service.CompraService;
 
 @RestController
 @RequestMapping("/compra")
 public class CompraResource {
 	
+	@Autowired
+	private CompraService compraService;
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public void Compra(@RequestBody CompraDTO compra) {
-		
+		compraService.ralizaCompra(compra);
 	}
 
 }
--- loja/src/main/java/br/com/microservico/loja/service/CompraService.java
+++ loja/src/main/java/br/com/microservico/loja/service/CompraService.java
@@ -1,5 +1,24 @@
 package br.com.microservico.loja.service;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.microservico.loja.dto.CompraDTO;
+import br.com.microservico.loja.dto.InfoFornecedorDTO;
+
 public class CompraService {
+
+	public void ralizaCompra(CompraDTO compra) {
+		
+		RestTemplate client = new RestTemplate();
+		
+		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
+				"http://localhost:8081/info/"+compra.getEndereco().getEstado(), 
+				HttpMethod.GET, null, 
+				InfoFornecedorDTO.class
+		);
+
+	}
 
 }
