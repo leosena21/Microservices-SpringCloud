5d060164c59e446db8b4f6feb01efcde8be4dc27
Leonardo <leeosena21@gmail.com>
2021-09-13 14:52:35 -0300

    Utilizando o Feign para fazer o acesso aos microservicos

--- loja/src/main/java/br/com/microservico/loja/service/CompraService.java
+++ loja/src/main/java/br/com/microservico/loja/service/CompraService.java
@@ -1,12 +1,9 @@
 package br.com.microservico.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.microservico.loja.client.FornecedorClient;
 import br.com.microservico.loja.dto.CompraDTO;
 import br.com.microservico.loja.dto.InfoFornecedorDTO;
 
@@ -14,27 +11,13 @@ import br.com.microservico.loja.dto.InfoFornecedorDTO;
 public class CompraService {
 	
 	@Autowired
-	private RestTemplate client;
-	
-	@Autowired
-	private DiscoveryClient eurekaClient;
+	private FornecedorClient fornecedorClient;
 
 	public void ralizaCompra(CompraDTO compra) {
 		
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://fornecedor/info/"+compra.getEndereco().getEstado(), 
-				HttpMethod.GET, null, 
-				InfoFornecedorDTO.class
-		);
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 		
-		eurekaClient.getInstances("fornecedor").stream()
-		.forEach(fornecedor -> {
-			System.out.println("localhost:"+fornecedor.getPort());
-
-		});
-		
-		System.out.println(exchange.getBody().getEndereco());
-
+		System.out.println(info.getEndereco());
 	}
 
 }
