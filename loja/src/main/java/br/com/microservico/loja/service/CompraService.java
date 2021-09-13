8113dca9b98f871ba393924749be3368145a8f60
Leonardo <leeosena21@gmail.com>
2021-09-13 14:34:39 -0300

    Exibindo instancias de fornecedor

--- loja/src/main/java/br/com/microservico/loja/service/CompraService.java
+++ loja/src/main/java/br/com/microservico/loja/service/CompraService.java
@@ -1,6 +1,7 @@
 package br.com.microservico.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,9 @@ public class CompraService {
 	
 	@Autowired
 	private RestTemplate client;
+	
+	@Autowired
+	private DiscoveryClient eurekaClient;
 
 	public void ralizaCompra(CompraDTO compra) {
 		
@@ -22,6 +26,12 @@ public class CompraService {
 				HttpMethod.GET, null, 
 				InfoFornecedorDTO.class
 		);
+		
+		eurekaClient.getInstances("fornecedor").stream()
+		.forEach(fornecedor -> {
+			System.out.println("localhost:"+fornecedor.getPort());
+
+		});
 		
 		System.out.println(exchange.getBody().getEndereco());
 
